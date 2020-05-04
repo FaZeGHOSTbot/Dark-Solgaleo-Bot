1e1980fe70fb05707d17fe3a8dc36d87723c51ea
FaZeGHOSTbot <64667737+FaZeGHOSTbot@users.noreply.github.com>
2020-05-04 23:52:18 +0530

    k

--- index.js
+++ index.js
@@ -830,7 +830,7 @@ bot.on('message', message=>{
             case 'burn':
                if(message.member.roles.cache.some(role => role.name === 'Gym Leader')){
                if(!args[1]) return message.reply('Please specify the number of messages to be burned by HELLFIRE!')
-               message.channel.bulkDelete(args[1]+1);
+               message.channel.bulkDelete(args[1]);
                }
                break;
            
